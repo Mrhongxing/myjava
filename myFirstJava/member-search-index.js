c8a20087273c23cfd52639f63ee3abfa5b098a21
Mrhongxing <154480715+Mrhongxing@users.noreply.github.com>
2025-10-15 20:21:01 +0800

    add_java_doc

---
+++ myFirstJava/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"myFirstJava","l":"main(String[])","u":"main(java.lang.String[])","k":"6"},{"p":"<Unnamed>","c":"myFirstJava","l":"myFirstJava()","u":"%3Cinit%3E()","k":"3"}];updateSearchResults();
