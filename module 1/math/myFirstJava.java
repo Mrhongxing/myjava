91464d28b451ba5a4c4ad04975611d2b84eaf9ba
Mrhongxing <154480715+Mrhongxing@users.noreply.github.com>
2025-10-18 19:07:43 +0800

    changeProjectModules

--- module 1/math/myFirstJava.java
+++
@@ -1,8 +0,0 @@
-public class myFirstJava {
-    public static void main(String[] args){
-        String a = "sdsdsd";
-        byte b  = 99;
-        System.out.print(a);
-        System.out.print(b);
-    }
-}
