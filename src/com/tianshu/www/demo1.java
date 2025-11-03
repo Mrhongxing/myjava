a57f2db97abc194fa4b2223915d33a45d6cf0554
Mrhongxing <154480715+Mrhongxing@users.noreply.github.com>
2025-11-03 10:24:34 +0800

    myjava0.1

---
+++ src/com/tianshu/www/demo1.java
@@ -0,0 +1,15 @@
+package com.tianshu.www;
+import java.util.*;//导入java.util包下的所有类
+
+public class demo1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+        scanner.close();
+        Random random = new Random();
+        int randomNumber = random.nextInt(100); // 生成0到99之间的随机数
+        System.out.println("Random number: " + randomNumber);
+    }
+}
