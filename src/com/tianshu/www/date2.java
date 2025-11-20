38505b770be6dae150daec465494d06d1db813de
Mrhongxing <154480715+Mrhongxing@users.noreply.github.com>
2025-11-20 09:37:30 +0800

    Update date2.java

--- src/com/tianshu/www/date2.java
+++ src/com/tianshu/www/date2.java
@@ -6,9 +6,16 @@ public class date2 {
     public static void main(String[] args) {
         Calendar c = Calendar.getInstance();
         System.out.println(c);
-        int year = c.get(Calendar.YEAR);
-        int month = c.get(Calendar.MONTH) + 1; // Months are 0-based
-        int day = c.get(Calendar.DAY_OF_MONTH);
+        int year = c.get(Calendar.YEAR);//年份
+        int month = c.get(Calendar.MONTH) + 1; // Months are 0-based月份
+        int day = c.get(Calendar.DAY_OF_MONTH);//日
+        int hour_half = c.get(Calendar.HOUR);//12小时制
+        int hour = c.get(Calendar.HOUR_OF_DAY);//24小时制
+        int minute = c.get(Calendar.MINUTE);//分
+        int second = c.get(Calendar.SECOND);//秒
+        int week = c.get(Calendar.DAY_OF_WEEK);//星期
         System.out.println("Current date: " + year + "-" + month + "-" + day);
+        System.out.println("Current time: " + hour + ":" + minute + ":" + second);
+        System.out.println("Current week: " + week);
     }
 }
