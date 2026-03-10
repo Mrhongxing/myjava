a58a75e8cc5c511dcb7da8ac1eba0a4871b4785b
Hanting Wang <2696801449@qq.com>
2026-03-10 08:00:01 +0800

    Update yvshui.java

--- src/com/tianshu/www/yvshui.java
+++ src/com/tianshu/www/yvshui.java
@@ -1,36 +1,73 @@
 package com.tianshu.www;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class yvshui {
-    public static void isYvshui(int[] num) {
+    public static void isYvshui(int[] height) {
         int left = 0;
         int position = 0;
-        int middle = -1;
+        ArrayList<Integer> middle = new ArrayList<>();
         int right;
         int shui=0;
         boolean hasleft = false;
-        for (int i = 0; i < num.length; i++) {
-            if (hasleft && num[i] < left) {
-                right = num[i];
-                if (middle>0) {
-                shui += (i-position - 1) *(Math.min(right,left)-middle);
-                middle =-1;
-                position=i;
-                left = num[i];
-                hasleft =true;
-                }
-            } else if (left <= num[i]) {
-                left = num[i];
-                position = i;
-                hasleft=true;
-            } else {
-                middle = num[i];
+        int max = 0 ;
+        int maxPosition = 0;
+        for(int i =0;i<height.length;i++){
+            if(height[i]>max){
+                max = height[i];
+                maxPosition = i;
             }
+        }
+        for (int i = 0; i <= maxPosition; i++) {
+            if (hasleft && left<= height[i]) {
+                right = height[i];
+                for (int j = 0; j < middle.size(); j++) {
+                    if (middle.get(j) < left) {
+                        shui += left - middle.get(j);
+                    }
+                }
+                middle.clear();
+                left = height[i];
+                position = i;
+            } else if (left <= height[i]) {
+                left = height[i];
+                position = i;
+            } else {
+                middle.add(height[i]);
+                hasleft = true;
+            }
+        }
+        hasleft = false;
+         left = 0;
+         position = 0;
+        for (int i = height.length - 1; i >= maxPosition; i--) {
+            if (hasleft && left<= height[i]) {
+                right = height[i];
+                for (int j = 0; j < middle.size(); j++) {
+                    if (middle.get(j) < left) {
+                        shui += left - middle.get(j);
+                    }
+                }
+                middle.clear();
+                left = height[i];
+                position = i;
+            } else if (left <= height[i]) {
+                left = height[i];
+                position = i;
+            }else {
+                middle.add(height[i]);
+                hasleft = true;
+            }
+            
         }
         System.out.println(shui);
     }
 
     public static void main(String[] args) {
         int[] numbers = {0,1,0,2,1,0,1,3,2,1,2,1};
+        int[] numbers2 = {4,2,0,3,2,5};
         isYvshui(numbers);
+        isYvshui(numbers2);
     }
 }
