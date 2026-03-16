770349018261271b3ed7383ccf5c2afdc8b911d9
Mrhongxing <154480715+Mrhongxing@users.noreply.github.com>
2026-03-16 16:23:40 +0800

    Update kaitou.java

--- src/com/tianshu/www/kaitou.java
+++ src/com/tianshu/www/kaitou.java
@@ -9,25 +9,24 @@ public class kaitou {
         boolean hasdifferent = true;
         int y= 0;
         for (String list : lists) {
+            y = 0;
             if (arrayList.size()==0) {
                     for (int i = 0; i < list.length(); i++) {
                         arrayList.add(list.charAt(i));
                     }
                 }else {
             if (list.length()<arrayList.size()) {
-                for (int j = list.length(); j < arrayList.size(); j++) {
-                    arrayList.remove(j);
-                }
+                arrayList.subList(list.length(), arrayList.size()).clear();
             }
-            for(int i = 0; i < list.length(); i++) {
+            for(int i = 0; i < arrayList.size(); i++) {
                 hasdifferent = false;
-                y++;
+                
                 System.out.println(list.charAt(i) + " " + arrayList.get(i));
                     if (list.charAt(i) == arrayList.get(i)) {
                         if (hasdifferent) {
                             arrayList.remove(i);
                         }
-
+                        y++;
                     }else {
                         hasdifferent = true;
                         break;
@@ -45,7 +44,7 @@ public class kaitou {
 System.out.println(str1); // Hello
     }
     public static void main(String[] args) {
-        String[] lists = {"goge", "goae", "goae"};
+        String[] lists = {"goaesss", "goaesss", "goaesa"};
         kaitou kaitou = new kaitou();
         kaitou.qukaitou(lists);
     }
