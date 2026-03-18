c18d25be2b42d1fe020bbc32c20c643712373285
Mrhongxing <154480715+Mrhongxing@users.noreply.github.com>
2026-03-18 10:22:38 +0800

    Update hua.java

--- src/com/tianshu/www/hua.java
+++ src/com/tianshu/www/hua.java
@@ -13,24 +13,37 @@ public class hua {
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[] a = new int[n];
-        int price = 0;
-        int num = 0;
+        int[] count = new int[m];
+        int num = 1;
         for (int i = 0; i < n; i++) {
             a[i] = sc.nextInt();
         }
         Arrays.sort(a);
-        for (int i = 0; i < n; i++) {
-            if (price + a[i] < m) {
-                price += a[i];
-                num++;
-            } else {
-                if(price+a[i]/2<m){
-                    num++;
+        for (int j = 0; j < m; j++) {
+            
+
+
+
+
+            
+
+            int x = sc.nextInt();
+                int y = sc.nextInt();
+                int z = sc.nextInt();
+                int min = Arrays.binarySearch(a, x);
+                int max= Arrays.binarySearch(a, y);
+                for(int s = min ; s < max+1; s++){
+                    num = 1;
+                    for(int t = 0; t < z; t++){
+                        num=num*a[s];
+                    }
+                    count[j] = count[j] + num;
                 }
-                break;
-                
-            }
+            
         }
-        System.out.println(num);
+        for(int i = 0; i < m; i++){
+            System.out.println(count[i]);
+        }
     }
+
 }
